ba100e0e02c94a3ce4cd359d4c35304420faaecd
jain-dishika <87193284+jain-dishika@users.noreply.github.com>
2023-01-07 15:46:37 +0530

    videoCall final integration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Components/Footer';
+import VideoCall from './Components/VideoCall';
+
+function App() {
+  return (
+    <>
+    <VideoCall/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
